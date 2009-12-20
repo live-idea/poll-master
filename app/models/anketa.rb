a9503e1a4fb1d455b4941aa7372c0235d0f7cc0d
kiosan <obondar@gmail.com>
2009-12-20 11:34:23 +0200

    added reorder

--- app/models/anketa.rb
+++ app/models/anketa.rb
@@ -1,6 +1,6 @@
 class Anketa < ActiveRecord::Base
   belongs_to :poll
-  has_many :questions, :dependent => :destroy #звязок питань та видалення питань
+  has_many :questions, :order=>"position ASC", :dependent => :destroy #звязок питань та видалення питань
   has_many :results, :dependent => :destroy
   validates_presence_of :title, :code, :description
 end
