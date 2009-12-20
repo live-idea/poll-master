7c292fb89ea1afe7a8fe544ee65a6d0dffca524f
chornyy <chab4@meta.ua>
2009-12-20 14:05:43 +0200

    valid empty filed

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :result
+  validates_presence_of :body
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,11 @@
 class Question < ActiveRecord::Base
   belongs_to :anketa
   validates_presence_of :body
+
+  def validate
+    if (self.qtype == 'select')
+      self.errors.add(:variants, :variants_can_not_be_ampty) if self.variants.empty?
+    end
+  end
+  
 end
